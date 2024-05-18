6ab68e36bbb129cd1905b93a9ade7b95d3a2447e
sanjanaravindran <34573985+sanjanaravindran@users.noreply.github.com>
2024-05-18 06:36:28 +0100

    Adding some model files

---
+++ 05-2024_IGFModels/2024-05-15-IGF1Models.R
@@ -0,0 +1,251 @@
+# Load libraries
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidybayes, rstanarm, rethinking, cmdstanr, parameters, tidyverse, lmerTest, ggeffects, plyr, reshape2, rptR, viridis, cowplot, bayesplot, patchwork, ggpubr)
+
+# Set wd
+setwd("C:/Users/sanja/OneDrive/Desktop/PostDocStuff/IGF-1")
+
+# Read Data
+igf_lh_data <- read.csv('2024MARCH_IGF1DataCollection/2024May_FinalIGF-LHDataset.csv',  header = T, stringsAsFactors = F, fileEncoding="UTF-8-BOM")
+source("03-2024-IGF1DataAnalysis/useful_functions.R")
+
+# Only include Ashworth Data and data where Intra-Assay CV < 10
+igf_lh_data <- igf_lh_data %>%
+  filter(Lab =="Ashworth" & IntraCVPass==1)
+
+# 
+table(igf_lh_data$CapYear)
+table(igf_lh_data$Sex_R)
+table(igf_lh_data$Twin)
+
+# Temp dataset for rstanarm model 
+temp <- igf_lh_data
+
+# Convert relevant columns to factor 
+cols_f <- c("SexF", "Twin")
+temp <- convert_to_factor(temp, cols_f)
+
+# Rescale variables 
+cols_sc <- c("IGF1", "PopSize", "StorageTime")
+temp <- standardize_columns(temp, cols_sc)
+
+# IGF-1 Model
+mod_igf <- rstanarm::stan_lmer(IGF1 ~ SexF + Twin + PopSize_sc + (1|ELISARunDate) + (1|PlateNumber) + (1|BirthYear) + (1|MumID),
+                     cores=4, 
+                     seed=12345,
+                     # QR=TRUE,
+                     data=temp)
+
+# Check model in lme4
+mod_igf_lmer <-lmer(IGF1 ~ SexF + Twin + PopSize_sc + (1|ELISARunDate) + (1|PlateNumber) + (1|BirthYear) + (1|MumID),
+                     # cores=4, 
+                     # seed=12345,
+                     # QR=TRUE,
+                     data=temp)
+summary(mod_igf_lmer)
+
+# Model diagnostics
+color_scheme_set("mix-teal-purple")
+pp_check(mod_igf) + labs(subtitle="IGF-1")
+
+rhat(mod_igf) %>%
+  as.data.frame() %>%
+  filter(.[[1]] > 1.1 & .[[1]] < 0.99)
+
+mcmc_neff(neff_ratio(mod_igf), size = 2)
+
+# Extract posterior draws for later use
+posterior_igf <- as.array(mod_igf)
+
+# Plot predictors
+
+predictors <- as.array(mod_igf, pars = c("SexF1", "Twin1", "PopSize_sc"))
+p1 <- bayesplot::mcmc_areas(predictors, point_est = c("median"), prob = 0.5, prob_outer = 0.95) +
+  theme_cowplot() +
+  vline_0(color = "darkgray", linetype = 2) +
+  scale_x_continuous(limits=c(-100,10)) +
+  scale_y_discrete(labels=c("Population Size", "Twin:1", "Sex:Female"), limits = rev) 
+p1  
+
+# Extract variance components (median; note: estimates differ a fair bit when you take mean instead)
+posterior_igf_df <- as.data.frame(mod_igf)
+med_igf_df <- median_hdci(posterior_igf_df)
+p_value(mod_igf, method="hdi")
+
+# Extract variance components (mean)
+variance_table <- as.data.frame(VarCorr(mod_igf))
+variance_table <- add_prop_var(variance_table)
+
+# Getting model predictions (conditional effects)
+# Sex effect
+sex_eff <- temp %>%
+  modelr::data_grid(PopSize_sc = mean(PopSize_sc), 
+                    # StorageTime_sc = mean(StorageTime_sc), 
+                    SexF = unique(SexF), 
+                    Twin=0) 
+
+sex_eff <- convert_to_factor(sex_eff, cols_f)
+
+sex_preds <- add_epred_draws(mod_igf, newdata = sex_eff, re_formula = NA) %>% 
+  group_by(SexF)  %>% 
+  median_hdci(.epred, .width = .95) 
+
+p2 <- ggplot(sex_preds, aes(x = SexF)) +
+  geom_jitter(data = temp, aes(x=SexF, y = IGF1, colour=SexF), size = 1.5,  alpha=0.3, height=0) +
+  geom_pointrange(aes(ymin = .lower, ymax=.upper, y=.epred)) +
+  theme_cowplot() +
+  scale_colour_brewer(name = "Sex=Female", palette="Set2") +
+  ylab("IGF-1 Concentration\n(ng/ml)") + 
+  xlab("") +
+  scale_x_discrete(labels=c("Male", "Female")) +
+  panel_bg(fill = "gray95", color = NA) +
+  grid_lines(color = "white") +
+  theme(legend.text = element_text(size=10),
+        legend.title = element_text(size=10)) +
+  theme(legend.position="none")+
+  theme(axis.text.x = element_text(size = 14))
+
+p2
+
+
+# Twin effect
+twin_eff <- temp %>%
+  modelr::data_grid(PopSize_sc = mean(PopSize_sc),  
+                    Twin = unique(Twin), 
+                    SexF=1) 
+
+twin_eff <- convert_to_factor(twin_eff, cols_f)
+
+twin_preds <- add_epred_draws(mod_igf, newdata = twin_eff, re_formula = NA) %>% 
+  group_by(Twin)  %>% 
+  median_hdci(.epred, .width = .95) 
+
+p3 <- ggplot(twin_preds, aes(x = Twin)) +
+  geom_jitter(data = temp, aes(x=Twin, y = IGF1, colour=Twin), size = 1.5,  alpha=0.5, height=0) +
+  geom_pointrange(aes(ymin = .lower, ymax=.upper, y=.epred)) +
+  theme_cowplot() +
+  scale_colour_brewer(name = "Twin", palette="Pastel1") +
+  ylab("") + 
+  xlab("") +
+  scale_x_discrete(labels=c("Singleton", "Twin")) +
+  panel_bg(fill = "gray95", color = NA) +
+  grid_lines(color = "white") +
+  theme(legend.text = element_text(size=10),
+        legend.title = element_text(size=10))+
+  theme(legend.position="none") +
+  theme(axis.text.x = element_text(size = 14))
+
+p3
+
+
+# Pop effect
+pop_eff <- temp %>%
+  modelr::data_grid(PopSize_sc = modelr::seq_range(PopSize_sc, n = 200), 
+                    # StorageTime_sc = mean(StorageTime_sc), 
+                    SexF = 1, 
+                    Twin=0) 
+
+pop_eff <- convert_to_factor(pop_eff, cols_f)
+
+pop_preds <- add_epred_draws(mod_igf, newdata = pop_eff, re_formula = NA, ndraws=500) %>% 
+  group_by(PopSize_sc) 
+
+pop_preds_med <- add_epred_draws(mod_igf, newdata = pop_eff, re_formula = NA) %>% 
+  group_by(PopSize_sc) %>%
+  median_hdci(.epred, .width = .95) 
+
+p4 <- ggplot(pop_preds, aes(x = PopSize_sc, y = IGF1)) +
+  geom_jitter(data = temp, size = 1.5,  alpha=0.2, height=0, width=0.1, color = "#3E285C") +
+  geom_line(aes(y = .epred, group = .draw), alpha=1/15, color = "#3E285C")  +
+  geom_line(data=pop_preds_med,  aes(y=.epred), linewidth=1.2, colour="#9457EB", alpha=0.8) +
+  theme_cowplot() +
+  scale_colour_brewer(name = "Population Size") +
+  ylab("") + 
+  xlab("Population Size") +
+  panel_bg(fill = "gray95", color = NA) +
+  grid_lines(color = "white") +
+  theme(legend.text = element_text(size=10),
+        legend.title = element_text(size=10))
+
+p4
+
+# Arrange the plots
+plot1 <- cowplot::plot_grid(p1 , 
+                            nrow = 1,
+                            labels = c("A"),
+                            align = "h")
+
+plot1
+ggsave("./IGF1_Writeup/Figures/IGF1_ForestPlot.tiff", plot1, dpi=600, width=4, height=3, bg="white")
+
+plot2 <- cowplot::plot_grid(p2, 
+                   p3 , 
+                   p4 ,
+                   nrow = 1,
+                   labels = c("B", "C", "D"),
+                   align = "h")
+
+plot2
+
+ggsave("./IGF1_Writeup/Figures/IGF1_SexLitSizePopSize.tiff", plot2, dpi=600, width=12, height=4, bg="white" )
+
+
+# Get the tables
+
+modelsummary(mod_igf, statistic = "conf.int", output="./IGF1_Writeup/Tables/IGFModelTable.docx")
+
+
+# Extra figs 
+# Histogram per sex
+means_sex <- ddply(igf_lh_data, "Sex_R", summarise, grp.mean=mean(IGF1))
+means_sex
+
+# Histogram
+plot_igf_sex <- ggplot(data=igf_lh_data, aes(x=IGF1, color=Sex_R)) +
+  geom_histogram(fill="white", alpha=0.5, position="dodge")+
+  geom_vline(data=means_sex, aes(xintercept=grp.mean, color=Sex_R),linetype="dashed") +
+  scale_color_manual(labels=c("Female", "Male"), values = c("purple","orange")) +
+  theme(legend.title=element_blank(),
+        text = element_text(size = 16))
+
+# Histogram per twin
+means_twin <- ddply(igf_lh_data, "Twin", summarise, grp.mean=mean(IGF1))
+means_twin
+
+# Histogram
+plot_igf_twins <- ggplot(data=igf_lh_data, aes(x=IGF1, color=factor(Twin))) +
+  geom_histogram(fill="white", alpha=0.5, position="dodge")+
+  geom_vline(data=means_twin, aes(xintercept=grp.mean, color=factor(Twin)),linetype="dashed") +
+  scale_color_manual(labels=c("Singleton", "Twin"), values = c("darkgreen", "brown")) +
+  theme(legend.title=element_blank(), 
+        text = element_text(size = 16))
+
+plot_igf_sex+plot_igf_twins
+
+plot3 <- cowplot::plot_grid(plot_igf_sex, 
+                            plot_igf_twins , 
+                            nrow = 1,
+                            labels = c("A", "B"),
+                            align = "h")
+
+plot3
+
+ggsave("./IGF1_Writeup/Figures/IGF1_SexLitSizeHistograms.tiff", plot3, dpi=600, width=12, height=4, bg="white" )
+
+
+# Plot variance components as stacked bar plot
+# 
+# prop_var
+# prop_var_table <- variance_table %>%
+#   select(grp, prop_variance) %>%
+#   arrange(desc(prop_variance))
+# prop_var_table$Trait = c("IGF1")
+# 
+# prop_var <- ggplot(prop_var_table, aes(fill=factor(grp, levels=c("BirthYear", "PlateNumber", "MumID", "ELISARunDate", "Residual")), y=prop_variance, x=Trait)) + 
+#   geom_bar(position="fill", stat="identity") +
+#   scale_fill_viridis(option="D", discrete = TRUE)  +
+#   theme_cowplot() +
+#   labs(x="IGF-1 Levels", y = "Proportion of Variance") +
+#   theme(axis.text=element_text(size=12), axis.title=element_text(size=12), axis.text.x=element_blank())+
+#   theme(legend.title = element_blank(), legend.text=element_text(size=11)) 
+
---
+++ 05-2024_IGFModels/2024-05-15-IGF1_MorphoTraits_Models.R
@@ -0,0 +1,170 @@
+# Load libraries
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(posterior, parameters, insight, see, tidybayes, rethinking, rstanarm, cmdstanr, tidyverse, lmerTest, ggeffects, plyr, reshape2, rptR, viridis, cowplot, bayesplot, patchwork, lubridate)
+
+# Set wd
+setwd("C:/Users/sanja/OneDrive/Desktop/PostDocStuff/IGF-1")
+
+# Read Data
+igf_lh_data <- read.csv('2024MARCH_IGF1DataCollection/2024May_FinalIGF-LHDataset.csv',  header = T, stringsAsFactors = F, fileEncoding="UTF-8-BOM")
+source("03-2024-IGF1DataAnalysis/useful_functions.R")
+
+# Only include Ashworth Data and data that passed intra-assay CV < 10% (14 samples failed i.e. 2% of samples)
+igf_lh_data <- igf_lh_data %>%
+  filter(Lab =="Ashworth" & IntraCVPass == 1)
+
+# # Temp dataset for stan models 
+temp <- igf_lh_data
+
+# Subset relevant columns
+temp <- temp %>%
+  select(ID, Weight, ForeLeg, Survival, BredAsAYearling, IGF1, SexF, Twin, PopSize, BirthYear, MumID, ELISARunDate, PlateNumber, DaysSinceBirth, BirthWt) 
+
+# Split data into different subsets (weight, foreleg, growth)
+temp_w <- temp %>% select(-ForeLeg, -Survival, -BredAsAYearling, -BirthWt, -DaysSinceBirth) %>% drop_na()
+temp_f <- temp %>% select(-Weight, -Survival, -BredAsAYearling, -BirthWt, -DaysSinceBirth) %>% drop_na()
+temp_g <- temp %>% filter(DaysSinceBirth > 20) %>% select(-ForeLeg, -Survival, -BredAsAYearling) %>% drop_na()
+
+# # Convert cols to numeric
+cols_n <- c("ELISARunDate", "PlateNumber", "BirthYear", "MumID")
+temp_w <- convert_to_num_fac(temp_w, cols_n)
+temp_f <- convert_to_num_fac(temp_f, cols_n)
+temp_g <- convert_to_num_fac(temp_g, cols_n)
+
+# Rescale variables
+temp_w <- standardize_columns(temp_w, c("Weight", "IGF1", "PopSize"))
+temp_f <- standardize_columns(temp_f, c("ForeLeg", "IGF1", "PopSize"))
+temp_g <- standardize_columns(temp_g, c("Weight", "IGF1", "PopSize", "DaysSinceBirth", "BirthWt"))
+
+
+# Prepare data list to pass to stan model
+data_weight <- prepare_data_list(temp_w)
+data_foreleg <- prepare_data_list(temp_f)
+data_growth <- prepare_data_list(temp_g)
+
+# Run the model using cmdstanr
+file_w <- here::here("03-2024-IGF1DataAnalysis/StanModel_IGF_Weight.stan")
+fit_mod_weight <- run_stan_model(file_w, data_weight)
+
+file_f <- here::here("03-2024-IGF1DataAnalysis/StanModel_IGF_ForeLeg.stan")
+fit_mod_foreleg <- run_stan_model(file_f, data_foreleg)
+
+file_g <- here::here("03-2024-IGF1DataAnalysis/StanModel_IGF_Growth.stan")
+fit_mod_growth <- run_stan_model(file_g, data_growth)
+
+
+# Check the model diagnostics
+fit_mod_list <- list(
+  fit_mod_weight, 
+  fit_mod_foreleg, 
+  fit_mod_growth
+)
+run_cmdstan_diagnose(fit_mod_list)
+# fit_mod_weight$cmdstan_diagnose()
+# fit_mod_foreleg$cmdstan_diagnose()
+# fit_mod_growth$cmdstan_diagnose()
+
+# Check posterior dist
+color_scheme_set("mix-teal-pink")
+(p1 <- generate_and_plot_ppc(fit_mod_weight, "Weight_rep", temp_w, temp_w$Weight, "Weight"))
+(p2 <- generate_and_plot_ppc(fit_mod_foreleg, "ForeLeg_rep", temp_f, temp_f$ForeLeg, "Foreleg"))
+(p3 <- generate_and_plot_ppc(fit_mod_growth, "Weight_rep", temp_g, temp_g$Weight, "Weight (Model accounting for Birth Weight)"))
+
+# Exrtact samples 
+# Add "true" IGF values estimated in model to original dataset with observed values to compare against
+temp_w <- process_fit_mod(fit_mod_weight, temp_w)
+temp_f <- process_fit_mod(fit_mod_foreleg, temp_f)
+temp_g <- process_fit_mod(fit_mod_growth, temp_g)
+
+# Plotting
+# Scatter-plot of IGF_true and observed IGF values 
+plot_violin(temp_w, "Weight")
+plot_violin(temp_f, "Foreleg Length")
+plot_violin(temp_g, "Weight (Model accounts for variation in birth weight)")
+
+# Compare means and variances of IGF_obs vs IGF_true
+var(temp_w$IGF1_sc)
+var(temp_w$IGF_true)
+cor.test(temp_w$IGF_true, temp_w$IGF1_sc)
+
+var(temp_f$IGF1_sc)
+var(temp_f$IGF_true)
+cor.test(temp_f$IGF_true, temp_f$IGF1_sc)
+
+var(temp_g$IGF1_sc)
+var(temp_g$IGF_true)
+cor.test(temp_g$IGF_true, temp_g$IGF1_sc)
+
+# Get posterior draws of weight and compare with observed
+# bayesplot::mcmc_intervals(fit_mod_weight$draws(), pars = c("beta_IGF", "beta_SexF"))
+post_weight <- process_fit_linear_model(fit_mod_weight, temp_w, 
+                                        c("alpha", "beta_IGF", "beta_SexF", "beta_Twin", "beta_PopSize"), 
+                                        "Weight")
+
+post_foreleg <- process_fit_linear_model(fit_mod_foreleg, temp_f, 
+                                         c("alpha", "beta_IGF", "beta_SexF", "beta_Twin", "beta_PopSize"), 
+                                         "ForeLeg")
+
+post_growth <- process_fit_growth_model(fit_mod_growth, temp_g, 
+                                        c("alpha", "beta_IGF", "beta_SexF", "beta_Twin", "beta_PopSize", "beta_DSB", "beta_BirthWt"), 
+                                        "Weight")
+
+#Plotting
+(plot_weight <- ggplot(data= temp_w, aes(x=IGF_true, y=Weight)) +
+  geom_point(colour="#FFBA52FF", alpha=0.6) +
+  geom_line(data=post_weight$summary_500draws_mu,  aes(y=value, group=draw), colour="#E683A9FF", alpha=1/15) +
+  geom_line(data=post_weight$mu_median,  aes(y=value), size=1.2, colour="#FFBA52FF", alpha=0.8) +
+  theme_cowplot() +
+  ylab("August Body Weight (in kg)") + 
+  xlab("Normalized True IGF-1 Concentration") +
+  scale_x_continuous(n.breaks=6) +
+  panel_bg(fill = "gray95", color = NA) +
+  grid_lines(color = "white") +
+  theme(legend.text = element_text(size=10),
+        legend.title = element_text(size=10)))
+
+
+#Plotting
+(plot_foreleg <- ggplot(data= temp_f, aes(x=IGF_true, y=ForeLeg)) +
+  geom_point(colour="#949398FF", alpha=0.6) +
+  geom_line(data=post_foreleg$summary_500draws_mu,  aes(y=value, group=draw), colour="#F4DF4EFF", alpha=1/15) +
+  geom_line(data=post_foreleg$mu_median,  aes(y=value), size=1.2, colour="#949398FF", alpha=0.8) +
+  theme_cowplot() +
+  ylab("August Foreleg Length (in cm)") + 
+  xlab("Normalized True IGF-1 Concentration") +
+  scale_x_continuous(n.breaks=6) +
+  panel_bg(fill = "gray95", color = NA) +
+  grid_lines(color = "white") +
+  theme(legend.text = element_text(size=10),
+        legend.title = element_text(size=10)))
+
+
+#Plotting
+(plot_growth <- ggplot(data= temp_g, aes(x=IGF_true, y=Weight)) +
+  geom_point(colour="#2C5F2D", alpha=0.6) +
+  geom_line(data=post_growth$summary_500draws_mu,  aes(y=value, group=draw), colour="#97BC62FF", alpha=1/15) +
+  geom_line(data=post_growth$mu_median,  aes(y=value), size=1.2, colour="#2C5F2D", alpha=0.8) +
+  theme_cowplot() +
+  ylab("August Body Weight (in kg)") + 
+  xlab("Normalized True IGF-1 Concentration") +
+  scale_x_continuous(n.breaks=6) +
+  panel_bg(fill = "gray95", color = NA) +
+  grid_lines(color = "white") +
+  ggtitle("Model accounting for variation in birth weight") +
+  theme(legend.text = element_text(size=10),
+        legend.title = element_text(size=10),
+        plot.title = element_text(size = 14, face="plain")))
+
+# Model summary
+# List the predictors for each model
+predictors_weight <- c("alpha", "beta_IGF", "beta_SexF", "beta_Twin", "beta_PopSize", "sigma_e", "sigma_e1", "sigma_t", "sigma_u", "sigma_v", "sigma_w")
+predictors_foreleg <- c("alpha", "beta_IGF", "beta_SexF", "beta_Twin", "beta_PopSize", "sigma_e", "sigma_e1", "sigma_t", "sigma_u", "sigma_v", "sigma_w")
+predictors_growth <- c("alpha", "beta_IGF", "beta_SexF", "beta_Twin", "beta_PopSize", "beta_DSB", "beta_BirthWt", "sigma_e", "sigma_e1", "sigma_t", "sigma_u", "sigma_v", "sigma_w")
+
+# Call the function for each model
+full_post_weight <- summarize_model(fit_mod_weight, predictors_weight)
+full_post_foreleg <- summarize_model(fit_mod_foreleg, predictors_foreleg)
+full_post_growth <- summarize_model(fit_mod_growth, predictors_growth)
+
+library(flextable)
+flextable(full_post_weight)
